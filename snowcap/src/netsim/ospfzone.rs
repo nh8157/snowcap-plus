ad19babc862090ff441b6923af861b0a0913b81c
PPSh-1895 <sl7553@nyu.edu>
2022-08-17 18:14:01 +0800

    8.17.2

--- snowcap/src/netsim/ospfzone.rs
+++ snowcap/src/netsim/ospfzone.rs
@@ -6,17 +6,29 @@ use std::collections::{HashMap,HashSet};
 use crate::netsim::{
     AsId, ConfigError, ForwardingState, LinkWeight, NetworkError, Prefix, RouterId,IgpNetwork
 };
-pub fn find_OSPF_strict_zone (network:&Network)-> Vec<Vec<RouterId>>{
+pub fn find_ospf_strict_zone (network:&Network)-> Vec<Vec<RouterId>>{
     fn judge(router1:RouterId,router2:RouterId, graph:&IgpNetwork)->bool{
+        println!("Judge: Router1 is {}. Router2 is {}.",router1.index(),router2.index());
         let possible_paths=all_simple_paths(graph, router1, router2,0,None);
         let (size,trash) = possible_paths.size_hint();
         let possible_paths_vec:Vec<HashSet<RouterId>>=possible_paths.collect();
         if size==0{
+            println!("size is 0");
             return false;
         }
-        let mut temp_intersect=&possible_paths_vec.clone()[0];
+        let mut temp_intersect=possible_paths_vec[0].clone();
+        let mut path_cnt=0;
         for path in possible_paths_vec{
-            temp_intersect.intersection(&path);
+            path_cnt=path_cnt+1;
+            for temp_router in &path{
+                println!("{} in path {}",temp_router.index(),path_cnt);
+            }
+            println!("");
+            let this_intersection=temp_intersect.intersection(&path);
+            let mut next_intersect=HashSet::new();
+            for intersected_RouterId in this_intersection{
+                next_intersect.insert(*intersected_RouterId);
+            }
         }
         let final_size=temp_intersect.len();
         if final_size>0{
@@ -29,18 +41,21 @@ pub fn find_OSPF_strict_zone (network:&Network)-> Vec<Vec<RouterId>>{
 
     let mut zones:Vec<Vec<RouterId>>=Vec::new();
     let mut temp_outside:Vec<RouterId>=network.get_routers();
+    let mut zone_cnt=0;
     while temp_outside.len()>0{
+        zone_cnt=zone_cnt+1;
         let mut next_outside: Vec<RouterId>=Vec::new();
         let router1=temp_outside[0];
         let mut temp_zone:Vec<RouterId>=Vec::new();
         temp_zone.push(router1);
         for router2 in temp_outside.iter(){
-            if *router2!=router1{
+            if router2.index()!=router1.index(){
                 let mut result = judge(router1, *router2, network.get_topology());
                 if result==true{
                     temp_zone.push(*router2);
                 }
                 else{
+                    //println!("Router {} is not in zone {}",router2.index(),zone_cnt);
                     next_outside.push(*router2);
                 }
             }
--- snowcap/src/test/test_ospf_zone.rs
+++ snowcap/src/test/test_ospf_zone.rs
@@ -3,7 +3,7 @@ use crate::netsim::router::Router;
 use crate::netsim::network::Network;
 use petgraph::algo::all_simple_paths;
 use std::collections::{HashMap,HashSet};
-use crate::netsim::ospfzone::find_OSPF_strict_zone;
+use crate::netsim::ospfzone::find_ospf_strict_zone;
 use crate::netsim::{
     AsId, ConfigError, ForwardingState, LinkWeight, NetworkError, Prefix, RouterId,
 };
@@ -12,16 +12,41 @@ use crate::netsim::{
 #[test]
 fn test_ospf(){
     let mut this_network=Network::new();
-    let A_id = this_network.add_router("A");
-    let B_id = this_network.add_router("B");
-    let C_id = this_network.add_router("C");
-    let D_id = this_network.add_router("D");
-    this_network.add_link(A_id,B_id);
-    this_network.add_link(A_id,C_id);
-    this_network.add_link(A_id,D_id);
-    this_network.add_link(B_id,C_id);
-    this_network.add_link(B_id,D_id);
-    this_network.add_link(C_id,D_id);
-    let mut zones=find_OSPF_strict_zone(&this_network);
-    println!("here!");
+    let a_id = this_network.add_router("A");
+    let b_id = this_network.add_router("B");
+    let c_id = this_network.add_router("C");
+    let d_id = this_network.add_router("D");
+    this_network.add_link(a_id,b_id);
+    this_network.add_link(a_id,c_id);
+    this_network.add_link(a_id,d_id);
+    this_network.add_link(b_id,c_id);
+    this_network.add_link(b_id,d_id);
+    this_network.add_link(c_id,d_id);
+    let a_id = this_network.get_router_id("A").unwrap();
+    let b_id = this_network.get_router_id("B").unwrap();
+    let c_id = this_network.get_router_id("C").unwrap();
+    let d_id = this_network.get_router_id("D").unwrap();
+    let mut c = Config::new();
+    c.add(ConfigExpr::IgpLinkWeight { source: a_id, target: b_id, weight: 10.0 }).unwrap();
+    c.add(ConfigExpr::IgpLinkWeight { source: b_id, target: a_id, weight: 10.0 }).unwrap();
+    c.add(ConfigExpr::IgpLinkWeight { source: a_id, target: c_id, weight: 10.0 }).unwrap();
+    c.add(ConfigExpr::IgpLinkWeight { source: c_id, target: a_id, weight: 10.0 }).unwrap();
+    c.add(ConfigExpr::IgpLinkWeight { source: a_id, target: d_id, weight: 10.0 }).unwrap();
+    c.add(ConfigExpr::IgpLinkWeight { source: d_id, target: a_id, weight: 10.0 }).unwrap();
+    c.add(ConfigExpr::IgpLinkWeight { source: b_id, target: c_id, weight: 10.0 }).unwrap();
+    c.add(ConfigExpr::IgpLinkWeight { source: c_id, target: b_id, weight: 10.0 }).unwrap();
+    c.add(ConfigExpr::IgpLinkWeight { source: b_id, target: d_id, weight: 10.0 }).unwrap();
+    c.add(ConfigExpr::IgpLinkWeight { source: d_id, target: b_id, weight: 10.0 }).unwrap();
+    c.add(ConfigExpr::IgpLinkWeight { source: c_id, target: d_id, weight: 10.0 }).unwrap();
+    c.add(ConfigExpr::IgpLinkWeight { source: d_id, target: c_id, weight: 10.0 }).unwrap();
+    this_network.set_config(&c).unwrap();
+    let mut zones=find_ospf_strict_zone(&this_network);
+    let x = true;
+    let mut zone_cnt=1;
+    for zone in zones{
+        for router in zone{
+            println!("Router {} in zone {}",router.index(),zone_cnt);
+        }
+        zone_cnt=zone_cnt+1;
+    }
 }
